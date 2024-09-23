f257fd858aac75b0684897c563d1f0e992071cf9
mar <anvar.muslimov.work@gmail.com>
2024-09-23 17:39:22 +0600

    feat: added markup and styles to root screen

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -6,7 +6,7 @@ import { icons } from '../../constants'
 const TabIcon = ({ icon, color, name, focused }) => {
   return (
     <View className="justify-center items-center gap-2">
-      <Image source={icon} resizeMethod="contain" tintColor={color} className="w-6 h-6" />
+      <Image source={icon} resizeMode="contain" tintColor={color} className="w-6 h-6" />
       <Text
         className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}
         style={{ color: color }}
