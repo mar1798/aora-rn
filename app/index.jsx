b71a39a6c2ed7659d559a4799346e869754e64cc
mar <anvar.muslimov.work@gmail.com>
2024-09-23 16:04:22 +0600

    feat: added images and fonts

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Aora!</Text>
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar style="auto" />
     </View>
   )
